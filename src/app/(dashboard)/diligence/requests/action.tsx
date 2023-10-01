87bc6913c14cf281d1b9349345c36908f7be57a4
Jesuloba <jesulobajohn@gmail.com>
2023-10-01 14:10:48 +0100

    upload and result pop out  modals done

--- src/app/(dashboard)/diligence/requests/action.tsx
+++ src/app/(dashboard)/diligence/requests/action.tsx
@@ -5,6 +5,11 @@ import { useState } from "react";
 import { FileDisplay } from "@/components/customdialog/fileDisplay";
 import { useRequest } from "@/hooks";
 import { RequestVerifyForm } from "@/components/features/fileUpload/requestVerifyForm";
+import { useMediaQuery } from "@/hooks";
+import Drawer from "react-modern-drawer";
+import { Button } from "@/components/ui/button";
+import Draggable from "react-draggable";
+import { X } from "lucide-react";
 
 export const ActionButton = ({
   status,
@@ -31,6 +36,8 @@ export const CompletedDialog = ({ requestId }: { requestId: string }) => {
   const requestDocument = useLazyGetRequestDocumentQuery(requestId);
   const documents = requestDocument.data?.data.data;
 
+  const matches = useMediaQuery("(min-width: 768px)");
+
   const cancelResultModal = () => {
     setOpen(false);
   };
@@ -40,35 +47,91 @@ export const CompletedDialog = ({ requestId }: { requestId: string }) => {
   };
 
   return (
-    <Dialog
-      dialogType="normal"
-      actionText="Done"
-      open={open}
-      setter={setOpen}
-      footer={true}
-      triggerText="See result"
-      triggerVariant={{
-        variant: "link",
-        size: "link",
-      }}
-      triggerClassName="underline"
-      title={
-        requestDocument.isLoading && !requestDocument.isSuccess
-          ? "Loading..."
-          : "Verification Successful"
-      }
-      action={doneAction}
-    >
-      {documents && (
-        <FileDisplay
-          fileName={documents[0].name}
-          type={documents[0].type}
-          link={documents[0].link}
-          date={documents[0].createdAt}
-          description={documents[0].description}
-        />
+    <>
+      {matches ? (
+        <Dialog
+          dialogType="normal"
+          actionText="Done"
+          open={open}
+          setter={setOpen}
+          footer={true}
+          triggerText="See result"
+          triggerVariant={{
+            variant: "link",
+            size: "link",
+          }}
+          triggerClassName="underline"
+          title={
+            requestDocument.isLoading && !requestDocument.isSuccess
+              ? "Loading..."
+              : "Verification Successful"
+          }
+          action={doneAction}
+        >
+          {documents && (
+            <FileDisplay
+              fileName={documents[0].name}
+              type={documents[0].type}
+              link={documents[0].link}
+              date={documents[0].createdAt}
+              description={documents[0].description}
+            />
+          )}
+        </Dialog>
+      ) : (
+        <>
+          <Button
+            variant={"link"}
+            size={"link"}
+            className="underline"
+            onClick={() => setOpen(true)}
+          >
+            {"See result"}
+          </Button>
+          <Drawer
+            direction="bottom"
+            open={open}
+            onClose={cancelResultModal}
+            className="!h-fit rounded-t-3xl overflow-hidden"
+          >
+            <Draggable
+              axis="y"
+              handle=".handle"
+              onStop={(e, data) => {
+                if (data.y > 100) {
+                  cancelResultModal();
+                }
+              }}
+            >
+              <>
+                <div className="w-10 h-1 bg-[#959697] mx-auto mt-2 rounded-sm handle" />
+                <Button
+                  type="button"
+                  variant={"ghost"}
+                  size={"slim"}
+                  onClick={cancelResultModal}
+                  className="self-end block ml-auto mr-5 md:hidden"
+                >
+                  <X />
+                </Button>
+                <div className="flex flex-col gap-10 p-5">
+                  {documents && (
+                    <FileDisplay
+                      fileName={documents[0].name}
+                      type={documents[0].type}
+                      link={documents[0].link}
+                      date={documents[0].createdAt}
+                      description={documents[0].description}
+                    />
+                  )}
+                  <Button>Done</Button>
+                </div>
+              </>
+            </Draggable>
+          </Drawer>
+        </>
       )}
-    </Dialog>
+    </>
   );
 };
 
@@ -79,22 +142,71 @@ export const VerifiedDialog = ({ requestId }: { requestId: string }) => {
     setOpen(false);
   };
 
+  const matches = useMediaQuery("(min-width: 768px)");
+
   return (
-    <Dialog
-      dialogType="normal"
-      open={open}
-      setter={setOpen}
-      footer={false}
-      title={"Upload document"}
-      showCloseButton={true}
-      triggerText="Upload"
-      triggerVariant={{
-        variant: "link",
-        size: "link",
-      }}
-      triggerClassName="underline"
-    >
-      <RequestVerifyForm requestId={requestId} closeModal={cancelDialog} />
-    </Dialog>
+    <>
+      {matches ? (
+        <Dialog
+          dialogType="normal"
+          open={open}
+          setter={setOpen}
+          footer={false}
+          title={"Upload document"}
+          showCloseButton={true}
+          triggerText="Upload"
+          triggerVariant={{
+            variant: "link",
+            size: "link",
+          }}
+          triggerClassName="underline"
+        >
+          <RequestVerifyForm requestId={requestId} closeModal={cancelDialog} />
+        </Dialog>
+      ) : (
+        <>
+          <Button
+            variant={"link"}
+            size={"link"}
+            className="underline"
+            onClick={() => setOpen(true)}
+          >
+            {"Upload"}
+          </Button>
+          <Drawer
+            direction="bottom"
+            open={open}
+            onClose={cancelDialog}
+            className="!h-fit rounded-t-3xl overflow-hidden"
+          >
+            <Draggable
+              axis="y"
+              handle=".handle"
+              onStop={(e, data) => {
+                if (data.y > 100) {
+                  cancelDialog();
+                }
+              }}
+            >
+              <>
+                <div className="w-10 h-1 bg-[#959697] mx-auto mt-2 rounded-sm handle" />
+                <Button
+                  type="button"
+                  variant={"ghost"}
+                  size={"slim"}
+                  onClick={cancelDialog}
+                  className="self-end block ml-auto mr-5 md:hidden"
+                >
+                  <X />
+                </Button>
+                <div className="p-5">
+                  <RequestVerifyForm requestId={requestId} closeModal={cancelDialog} />
+                </div>
+              </>
+            </Draggable>
+          </Drawer>
+        </>
+      )}
+    </>
   );
 };
